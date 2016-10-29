73f94077d8ffa6adc32b72fa69060a1bcd5c4e24
Ben Lamptey <b.lamptey@catchdigital.com>
2016-10-29 19:37:20 +0100

    commented

--- docroot/sites/all/themes/smilee/tpl/page--front.tpl.php
+++ docroot/sites/all/themes/smilee/tpl/page--front.tpl.php
@@ -1,32 +1,32 @@
-<?php require_once(drupal_get_path('theme', 'smilee') . '/tpl/header.tpl.php'); ?>
-
-<?php if ($page['slider']): ?>
-  <?php print render($page['slider']); ?>
-<?php endif; ?>
-
-<?php if ($page['homepage_category']): ?>
-  <div class="block block-block block-main container">
-    <?php print render($page['homepage_category']); ?>
-  </div>
-<?php endif; ?>
-
-
-<?php if ($page['content']): ?>
-  <?php
-  if (!empty($tabs['#primary']) || !empty($tabs['#secondary'])):
-    print render($tabs);
-  endif;
-  print $messages;
-  unset($page['content']['system_main']['default_message']);
-  ?>
-
-  <?php print render($page['content']); ?>
-
-
-<?php endif; ?>
-<?php if ($page['section']): ?>
-  <?php print render($page['section']); ?>
-<?php endif; ?>
-
-
-<?php require_once(drupal_get_path('theme', 'smilee') . '/tpl/footer.tpl.php'); ?>
+<?php //require_once(drupal_get_path('theme', 'smilee') . '/tpl/header.tpl.php'); ?>
+<!---->
+<?php //if ($page['slider']): ?>
+<!--  --><?php //print render($page['slider']); ?>
+<?php //endif; ?>
+<!---->
+<?php //if ($page['homepage_category']): ?>
+<!--  <div class="block block-block block-main container">-->
+<!--    --><?php //print render($page['homepage_category']); ?>
+<!--  </div>-->
+<?php //endif; ?>
+<!---->
+<!---->
+<?php //if ($page['content']): ?>
+<!--  --><?php
+//  if (!empty($tabs['#primary']) || !empty($tabs['#secondary'])):
+//    print render($tabs);
+//  endif;
+//  print $messages;
+//  unset($page['content']['system_main']['default_message']);
+//  ?>
+<!---->
+<!--  --><?php //print render($page['content']); ?>
+<!---->
+<!---->
+<?php //endif; ?>
+<?php //if ($page['section']): ?>
+<!--  --><?php //print render($page['section']); ?>
+<?php //endif; ?>
+<!---->
+<!---->
+<?php //require_once(drupal_get_path('theme', 'smilee') . '/tpl/footer.tpl.php'); ?>
